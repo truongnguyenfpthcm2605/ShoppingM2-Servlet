eff5379898beeb54215b23cca44629ad21e3bfe6
truongnguyenvan <truongnguyenvan@altek.com.vn>
2023-08-08 10:16:12 +0700

    Up load Source, Completed connect With Sql Server

---
+++ src/main/java/com/m2m/shopping/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.m2m.shopping;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+        request.setAttribute("name","Nguyễn Văn Trường");
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
